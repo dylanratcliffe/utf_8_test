1cc1a6f2d199241056232536e78e83c8991e3c0f
beergeek <brett.gray@puppetlabs.com>
2016-06-27 11:42:52 +1000

    utf-8 generic

--- site/role/manifests/base.pp
+++ site/role/manifests/base.pp
@@ -1,18 +1,18 @@
 class role::base (
-  $ensure_utf_8_host = false,
-  $ensure_japanase_files = false,
-  $ensure_utf_8_users = false,
-  $ensure_utf_8_group = false,
-  $ensure_japanse_concat = false,
+  $ensure_utf_8_host   = false,
+  $ensure_utf_8_files  = false,
+  $ensure_utf_8_users  = false,
+  $ensure_utf_8_group  = false,
+  $ensure_utf_8_concat = false,
   $utf_8_notify_string = 'こんにちは',
 ) {
 
   class { 'profile::base':
     ensure_utf_8_host   => $ensure_utf_8_host,
-    ensure_japanase_files  => $ensure_japanase_files,
+    ensure_utf_8_files  => $ensure_utf_8_files,
     ensure_utf_8_users  => $ensure_utf_8_users,
     ensure_utf_8_group  => $ensure_utf_8_group,
-    ensure_japanse_concat  => $ensure_japanse_concat,
+    ensure_utf_8_concat => $ensure_utf_8_concat,
     utf_8_notify_string => $utf_8_notify_string,
   }
 }
