a922f3f647bf140773adff2b886f4e2e859238dc
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2016-07-08 16:49:51 +1000

    Added english

--- manifests/site.pp
+++ manifests/site.pp
@@ -9,4 +9,11 @@ site {
       Node['node1.puppet.vm'] => Utf_8::Component_one['ブランク_second'],
     },
   }
+
+  utf_8::application { 'English':
+    nodes => {
+      Node['node0.puppet.vm'] => Utf_8::Component_one['English_first'],
+      Node['node1.puppet.vm'] => Utf_8::Component_one['English_second'],
+    },
+  }
 }
