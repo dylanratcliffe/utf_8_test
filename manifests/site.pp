fd2fe99121afe12346baa2d99bec9cabef4abd80
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2016-07-11 11:15:23 +1000

    Rename

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,14 +3,14 @@ node default {
 }
 
 site {
-  utf_8::application { 'ブランク':
+  utf_8::app { 'ブランク':
     nodes => {
       Node['node0.puppet.vm'] => Utf_8::Component_one['ブランク_first'],
       Node['node1.puppet.vm'] => Utf_8::Component_one['ブランク_second'],
     },
   }
 
-  utf_8::application { 'English':
+  utf_8::app { 'English':
     nodes => {
       Node['node0.puppet.vm'] => Utf_8::Component_one['English_first'],
       Node['node1.puppet.vm'] => Utf_8::Component_one['English_second'],
