83d6a09ae0e164fe0d3e93e38eb6087c695c0670
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2016-07-13 12:26:49 +1000

    Commented out app definition

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,12 +3,12 @@ node default {
 }
 
 site {
-  utf_8::utf_8_application { 'ブランク':
-    nodes => {
-      Node['node0.puppet.vm'] => Utf_8::Component_one['ブランク_first'],
-      Node['node1.puppet.vm'] => Utf_8::Component_one['ブランク_second'],
-    },
-  }
+  # utf_8::utf_8_application { 'ブランク':
+  #   nodes => {
+  #     Node['node0.puppet.vm'] => Utf_8::Component_one['ブランク_first'],
+  #     Node['node1.puppet.vm'] => Utf_8::Component_one['ブランク_second'],
+  #   },
+  # }
 
   utf_8::application { 'English':
     nodes => {
