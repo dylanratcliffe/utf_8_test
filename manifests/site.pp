ac3945bdf9a7cfce1fba475f72ba900416e45fd2
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2016-07-14 11:49:40 +1000

    Added random message

--- manifests/site.pp
+++ manifests/site.pp
@@ -11,7 +11,8 @@ site {
   # }
 
   utf_8::app { 'English':
-    nodes => {
+    message => fqdn_rand_string('10'),
+    nodes   => {
       Node['node0.puppet.vm'] => Utf_8::Component_one['English_first'],
       Node['node1.puppet.vm'] => Utf_8::Component_one['English_second'],
     },
